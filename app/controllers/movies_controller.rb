cea2ca491c4e3c1025a12c0be1705ea2c0fd3f0f
Michael Pedersen <mpeders3@uncc.edu>
2015-02-26 18:52:21 -0400

    Chapter 4 Section 11 snapshot - added description text to seed movies.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,4 +7,28 @@ class MoviesController < ApplicationController
 		@movie = Movie.find(id)
 		# will render app/views/movies/show.html.haml
 	end
+	def new
+		#needs no data from the model to function
+		#render new template
+	end
+	def create
+		@movie = Movie.create!(params[:movie]);
+		flash[:notice]  = "#{@movie.title} was successfuly created!"
+		redirect_to movies_path
+	end
+	def edit
+		@movie = Movie.find params[:id]
+	end
+	def update
+		@movie = Movie.find params[:id]
+		@movie.update_attributes!(params[:movie])
+		flash[:notice] = "#{@movie.title} was successfuly updated!"
+		redirect_to movie_path (@movie)
+	end
+	def destroy
+		@movie = Movie.find(params[:id])
+		@movie.destroy
+		flash[:notice] = "Movie '#{@movie.title}' deleted."
+		redirect_to movies_path
+	end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,13 @@
 # Seed the RottenPotatoes DB with some movies.
 more_movies = [
   {:title => 'Aladdin', :rating => 'G',
-    :release_date => '25-Nov-1992'},
+    :release_date => '25-Nov-1992', :description => 'Aladdin Description Text'},
   {:title => 'When Harry Met Sally', :rating => 'R',
-    :release_date => '21-Jul-1989'},
+    :release_date => '21-Jul-1989', :description => 'When Harry Met Sally description text'},
   {:title => 'The Help', :rating => 'PG-13',
-    :release_date => '10-Aug-2011'},
+    :release_date => '10-Aug-2011', :description => 'The Help description text'},
   {:title => 'Raiders of the Lost Ark', :rating => 'PG',
-    :release_date => '12-Jun-1981'}
+    :release_date => '12-Jun-1981', :description => 'Raiders of the Lost Ark desciption text'}
 ]
 
 more_movies.each do |movie|
